67e1d748a2e115a795d3e4a74bf594ea8660cdd4
Iuliia <bieliaievays@gmail.com>
2016-06-02 17:31:30 +0200

    First commit

---
+++ sample/src/com/viewpagerindicator/sample/MainActivity.java
@@ -0,0 +1,73 @@
+package com.viewpagerindicator.sample;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.viewpagerindicator.library.CircleView;
+
+public class MainActivity extends FragmentActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int PAGE_COUNT = 5;
+
+    private ViewPager pager;
+    private PagerAdapter pagerAdapter;
+    private CircleView circleView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pager = (ViewPager) findViewById(R.id.pager);
+        pagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
+        pager.setAdapter(pagerAdapter);
+
+        pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int position) {
+                Log.d(TAG, "onPageSelected, position = " + position);
+            }
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset,
+                                       int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+
+        circleView = (CircleView) findViewById(R.id.circle_view);
+        circleView.setColorAccent(getResources().getColor(R.color.colorAccent));
+        circleView.setColorBase(getResources().getColor(R.color.colorPrimary));
+        circleView.setViewPager(pager);
+    }
+
+    private class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+
+        public MyFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return PageFragment.newInstance(position);
+        }
+
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;
+        }
+
+    }
+
+}
---
+++ sample/src/com/viewpagerindicator/sample/PageFragment.java
@@ -0,0 +1,51 @@
+package com.viewpagerindicator.sample;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Random;
+
+/**
+ * Created by iuliia on 29.05.16.
+ */
+public class PageFragment extends Fragment {
+
+    private static final String ARGUMENT_PAGE_NUMBER = "arg_page_number";
+
+    private int pageNumber;
+    private int backColor;
+
+    static PageFragment newInstance(int page) {
+        PageFragment pageFragment = new PageFragment();
+        Bundle arguments = new Bundle();
+        arguments.putInt(ARGUMENT_PAGE_NUMBER, page);
+        pageFragment.setArguments(arguments);
+        return pageFragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        pageNumber = getArguments().getInt(ARGUMENT_PAGE_NUMBER);
+
+        Random rnd = new Random();
+        backColor = Color.argb(40, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment, null);
+
+        TextView tvPage = (TextView) view.findViewById(R.id.tvPage);
+        tvPage.setText("Page " + pageNumber);
+        tvPage.setBackgroundColor(backColor);
+
+        return view;
+    }
+}
